98293aa00c49bc116c2274c3ade84496a98cbd8c
utente <utente@DESKTOP-A89FJ9S.lan>
2020-11-05 17:14:46 +0100

    Login Completo finito grazie, prego

--- LetteralMente/src/controller/Login.java
+++ LetteralMente/src/controller/Login.java
@@ -23,33 +23,19 @@ public class Login extends HttpServlet {
 
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		Utente u=new Utente();
 		u.setUsername(request.getParameter("username"));
 		u.setPassword(request.getParameter("password"));
 		if(Utility.leggiUtente().contains(u)) {
 			u=Utility.leggiUtente().get(Utility.leggiUtente().indexOf(u));
-			if(!u.getIsStaff()) {
-				request.getSession().setAttribute("UtenteLoggato", u);
-			}
-			else {
-				request.getSession().setAttribute("UtenteLoggato", u);
-				response.sendRedirect(""/*NOME DELLA PAGINA ADMIN*/);
-			}
-		}/*else
-		 	LOGIN FALLITO*/
-	}
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		for(Utente u: Utility.leggiUtente()) {
-			if(request.getParameter("username").equals(u.getUsername())&&request.getParameter("password").equals(u.getPassword()))
-				if(!u.getIsStaff()) {
-					request.getSession().setAttribute("UtenteLoggato", u);
-				}
-				else {
-					request.getSession().setAttribute("UtenteLoggato", u);
-					response.sendRedirect(""/*NOME DELLA PAGINA ADMIN*/);
-				}
-					
-		}
+			request.getSession().setAttribute("UtenteLoggato", u);
+			if(u.getIsStaff()) 
+				response.sendRedirect("/homeGestionale.jsp");		
+		}else
+		 	request.setAttribute("loginFallito", "errorLogin");
 	}
 
 }
