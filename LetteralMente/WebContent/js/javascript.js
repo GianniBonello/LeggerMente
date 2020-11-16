c94ea76da91779c3ae8939b32d2155cf5546b2a8
S4yron <sayron30@gmail.com>
2020-11-16 20:04:21 +0100

    controllo max caratteri nei form

--- LetteralMente/WebContent/js/javascript.js
+++ LetteralMente/WebContent/js/javascript.js
@@ -67,9 +67,9 @@ function login(){
 
       $(window).scroll(function() {
         if($(this).scrollTop() != 0) {
-          $('#go_top').fadeIn(); //faccio apparire il bottone
+          $('#go_top').fadeIn();
         } else {
-          $('#go_top').fadeOut();//Il bottone scompare
+          $('#go_top').fadeOut();
         }
         
       })
@@ -87,27 +87,16 @@ $("#capid").keyup(function() {
 
 
 $(document).ready(function(){
-  // Add smooth scrolling to all links
   $("nav a").on('click', function(event) {
-
-    // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
-      // Prevent default anchor click behavior
       event.preventDefault();
-
-      // Store hash
       var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
         scrollTop: $(hash).offset().top
       }, 800, function(){
-   
-        // Add hash (#) to URL when done scrolling (default click behavior)
         window.location.hash = hash;
       });
-    } // End if
+    } 
   });
 });
 
